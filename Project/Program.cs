2a5beec433ec297afcd7c351fdce875b2d46d052
Grokwik <frederic@bodin.site>
2017-01-05 12:48:06 +0100

    Test cases

--- Project/Program.cs
+++ Project/Program.cs
@@ -8,8 +8,21 @@ namespace Project
 {
     public class Solution
     {
-        public static void DoIt()
+        public IEnumerable<string> Filter(List<string> strings)
         {
+            /*
+             * From the parameter strings, Filter should return an enumeration having the following properties:
+             * -> Strings contained in the enumeration begin with the character 'L'
+             * -> The enumeration is sorted in alphabetical order.
+             * 
+             * Implement Filter using LINQ.As shown in the test code, your solution should
+             * work if the list of strings is modified after the call to Filter(do not use ToList()).
+             * 
+             * Example of output:
+             * Litch
+             * Lobster
+             */
+
         }
 
         static void Main(string[] args)
--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NFluent;
+using System.Collections.Generic;
 
 namespace Project
 {
@@ -7,9 +8,36 @@ namespace Project
     public class Tests
     {
         [TestMethod]
-        public void Test1()
+        public void TestRightContent()
         {
-            Solution.DoIt();
+            var strings = new List<string>();
+            var filteredStrings = new Solution().Filter(strings);
+            strings.Add("Gurt");
+            strings.Add("Lobster");
+            strings.Add("Litch");
+            strings.Add("Doe");
+            
+            Check.That(filteredStrings).Contains("Litch");
+            Check.That(filteredStrings).Contains("Lobster");
+            Check.That(filteredStrings).Not.Contains("Gurt");
+            Check.That(filteredStrings).Not.Contains("Doe");
+        }
+
+        [TestMethod]
+        public void TestRightOrder()
+        {
+            var strings = new List<string>();
+            var filteredStrings = new Solution().Filter(strings);
+            strings.Add("Gurt");
+            strings.Add("Lobster");
+            strings.Add("Litch");
+            strings.Add("Doe");
+
+            var expected = new List<string>();
+            foreach (var str in filteredStrings)
+                expected.Add(str);
+            Check.That(expected[0]).Equals("Litch");
+            Check.That(expected[1]).Equals("Lobster");
         }
     }
 }
